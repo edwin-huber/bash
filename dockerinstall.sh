c48257f30181cb671c753717708ca7e3f9361187
Edwin Huber <edwin-huber@users.noreply.github.com>
2017-07-04 21:31:30 +0200

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Install official NVIDIA driver package
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
